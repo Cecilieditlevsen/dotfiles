da0f150287804a421c3e8dd6788aace2f2151452
Cecilie Ditlevsen <cecilierd98@gmail.com>
2026-03-09 08:59:34 +0100

    Add fish plugins

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,9 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# Add Yabai functions to path
-set fish_function_path $fish_function_path $fish_function_path/yabai $fish_function_path/az
-
 # Override fish greeting
 set fish_greeting
 
@@ -20,12 +17,3 @@ zoxide init fish | source
 # Init starship prompt
 starship init fish | source
 
-# pnpm
-set -gx PNPM_HOME "/Users/cec/Library/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
-end
-# pnpm end
-
-# Added by Antigravity
-fish_add_path /Users/cec/.antigravity/antigravity/bin
